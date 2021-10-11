e361352fe317310bc62073d388723302a53a2cb5
someh <someh2705@gmail.com>
2021-10-11 22:00:05 +0900

    feat: Add MainViewModel in MainScreen

--- app/src/main/java/com/study/compose/page/main/MainScreen.kt
+++ app/src/main/java/com/study/compose/page/main/MainScreen.kt
@@ -10,13 +10,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.study.compose.navigation.Screen
 
 
 @Composable
-fun MainScreen(navController: NavController) {
+fun MainScreen(navController: NavController, viewModel: MainViewModel = hiltViewModel()) {
+    val state = viewModel.state.value
     Scaffold(
         backgroundColor = Color.White,
         topBar = {
