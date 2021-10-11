b85595c203306b6ff4b220ca4a886fc4be19941f
someh <someh2705@gmail.com>
2021-10-11 22:03:49 +0900

    feat: Add LayoutViewModel in LayoutScreen

--- app/src/main/java/com/study/compose/page/layout/LayoutScreen.kt
+++ app/src/main/java/com/study/compose/page/layout/LayoutScreen.kt
@@ -4,9 +4,12 @@ import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 
 @Composable
-fun LayoutScreen() {
+fun LayoutScreen(navController: NavController, viewModel: LayoutViewModel = hiltViewModel()) {
     Scaffold(
         backgroundColor = Color.White,
         topBar = {
@@ -20,5 +23,5 @@ fun LayoutScreen() {
 @Preview(showBackground = true)
 @Composable
 fun LayoutPagePreview() {
-    LayoutScreen()
+    LayoutScreen(navController = rememberNavController())
 }
